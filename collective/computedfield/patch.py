145f6629c53bf32bd4fc5fdf36a526f507c62e24
Sean Upton <sdupton@gmail.com>
2017-08-11 16:12:03 -0600

    More precise and careful monkey patch for plone.autoform: neither use exact classname (support subclasses, since we sometimes need that) nor drop all omitted fields; just remove computed fields from omission on plone.schemaeditor listings.

--- collective/computedfield/patch.py
+++ collective/computedfield/patch.py
@@ -1,12 +1,22 @@
 from plone.autoform import utils
 from plone.autoform.interfaces import OMITTED_KEY
 
+from field import ComputedField
 
-orig_mergedTaggedValuesForForm = utils.mergedTaggedValuesForForm
 
+try:
+    from plone.schemaeditor.browser.schema.listing import SchemaListing
 
-def mergedTaggedValuesForForm(schema, name, form):
-    if form.__class__.__name__ == 'SchemaListing' and name == OMITTED_KEY:
-        return {}
-    return orig_mergedTaggedValuesForForm(schema, name, form)
+    orig_mergedTaggedValuesForForm = utils.mergedTaggedValuesForForm
 
+    def mergedTaggedValuesForForm(schema, name, form):
+        result = orig_mergedTaggedValuesForForm(schema, name, form)
+        if issubclass(form.__class__, SchemaListing) and name == OMITTED_KEY:
+            for fieldname in result.keys():
+                field = schema[fieldname]
+                if isinstance(field, ComputedField):
+                    del(result[fieldname])
+        return result
+
+except ImportError:
+    pass
